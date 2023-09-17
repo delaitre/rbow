31045773c36b168b7bee1c5c027a89d572dc4f79
Jeremie Delaitre <jeremie.delaitre@gmail.com>
2023-09-17 15:03:29 +1200

    Add support for command line arguments
    Only support --example for now.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,29 @@
+use clap::Parser;
 use rbow::{config::Config, config::ConfigPath, stylize::Stylize};
 use std::{error::Error, io, io::prelude::*};
 
+/// A simple program colorizing its input based on regular expressions.
+///
+/// Regular expressions to match are loaded from `.rbow` files in parent directories.
+///
+/// Example usage: `cat my_log_file.txt | rbow`
+#[derive(Parser, Debug)]
+#[command(author, version, about)]
+struct Args {
+    /// Print an example .rbow file
+    #[arg(short, long)]
+    example: bool,
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
+    let args = Args::parse();
+
+    if args.example {
+        println!("{}", Config::example().serialized().unwrap());
+        return Ok(());
+    }
+
     let config = Config::load(ConfigPath::new(".", ".rbow"));
-    //let config = Config::example();
     let rules = config.as_rules();
 
     for line in io::stdin().lock().lines() {
